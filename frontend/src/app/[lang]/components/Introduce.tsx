3d214d9632be9bb8f52994a171e77998dee410f5
Phuc Huynh <baophuchuynhngoc@gmail.com>
2023-06-06 14:59:18 +0700

    fix: fix style Introduce Component

--- frontend/src/app/[lang]/components/Introduce.tsx
+++ frontend/src/app/[lang]/components/Introduce.tsx
@@ -45,54 +45,42 @@ export default function Introduce ({ data }: IntroduceProps) {
   const imgUrl = getStrapiMedia(data.picture.data.attributes.url);
   const imgBackgroundUrl = getStrapiMedia(data.imgBackground.data.attributes.url);
   const iconUrl = getStrapiMedia(data.icon.data.attributes.url);
-    console.log(data);
     return (
-        <section className="bg-primary-blue relative mb-[210px]">
-            <div className="">
-            <div className="bg-no-repeat bg-bottom bg-cover lg:bg-auto pt-[34px] pb-[59px]" style={{backgroundImage: `url(${imgBackgroundUrl})`,backgroundSize: "1372px 544px"}}>
-                <div className="container p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                    <div className="text-center mb-[40px]">
-                <HighlightedText
-                    text={data.title}
-                    tag="h2"
-                    className="text-h2 text-white font-bold leading-none leading-[81.5px] mb-[15px]"
-                    />
-                    <HighlightedText
-                    text={data.subTitle}
-                    tag="h4"
-                    className="text-h4 text-white font-semibold leading-none leading-[81.5px]"
-                    />
-                    </div>
-                    <div>
-                        <ul className="lg:w-[30%] list-disc text-white">
+        <section className="relative mb-[110px]">
+            <div className="bg-no-repeat bg-center lg:bg-right bg-cover lg:bg-auto pt-[34px] pb-[59px]" style={{backgroundImage: `url(${imgBackgroundUrl})`,backgroundSize: "1372px 544px"}}>
+                <div className="container grid grid-cols-1 lg:grid-cols-2 p-6 mx-auto sm:py-12 lg:py-24">
+                    <div className="">
+                        <div className="mb-[40px] lg:w-4/5 lg:text-left text-center">
+                        <HighlightedText
+                            text={data.title}
+                            tag="h2"
+                            className="text-h2 text-primary-blue font-bold leading-[72px]  mb-[15px]"
+                        />
+                        <HighlightedText
+                            text={data.subTitle}
+                            tag="h4"
+                            className="text-h4 text-primary-blue font-semibold "
+                        />
+                        </div>
+                        <ul className="list-disc text-primary-blue text-justify lg:w-2/3">
                             {data.paragraph.map((para,index) => (
                                 <li key={index} className="mb-[40px]">{para.description}</li>
                                 )
-                                
                                 )}
                         </ul>
-                        <div className="mt-2">
-                        <Link
-                            href={data.buttons.url}
-                            target={data.buttons.newTab ? "_blank" : "_self"}
-                            className="bg-white text-primary-blue text-ex font-semibold py-2 px-[40px] rounded-[39px] "
-                        >
-                            {data.buttons.text}
-                        </Link>
+                </div>
+                <div className="relative">
+                    <Image src={imgUrl} alt="" width={729} height={540} className="rounded-[12px] mt-[40px] w-full" />
+                    <div className="absolute -bottom-[20px] left-0">
+                        <div className="text-white py-[22px] px-[15px] bg-primary-brown rounded-[12px]">
+                            <p className="text-[20px] lg:text-[40px] font-bold w-fit mx-auto">1245</p>
+                            <p className="text-p lg:text-h4 w-fit">clients worldwide</p>
                         </div>
                     </div>
+                    <Image src={iconUrl} alt="" width={101} height={101} className="rounded-[12px] absolute bottom-[66px] -left-[50px]" />
                 </div>
-                <div className="lg:absolute lg:-bottom-8 lg:right-36">
-                    <Image src={imgUrl} alt="" width={729} height={540} className="rounded-[12px] " />
-                    <Image src={iconUrl} alt="" width={101} height={101} className="rounded-[12px] absolute -top-[40px] -left-[50px]" />
-                    <div className="absolute -bottom-[70px] -right-[36px]">
-                        <div className="text-white py-[32px] px-[15px] bg-primary-brown rounded-[12px]">
-                            <p className="text-[40px] font-bold w-fit mx-auto">1245</p>
-                            <p className="text-h4 w-fit">clients worldwide</p>
-                        </div>
-                    </div>
                 </div>
-            </div>
+
             </div>
 
         </section>
