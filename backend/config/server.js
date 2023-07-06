c553957fb648ee3fb384b6790d1ba98f55c5cf17
Huỳnh Ngọc Bảo Phúc <62460606+baophuchuynhngoc@users.noreply.github.com>
2023-07-06 09:31:45 +0700

    Add APP_KEYS

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',["jP8pb1lYsAhnmURarewxhA==","34xnLMYHY5jiU7ONTstTqQ=="]),
   },
   url: env('STRAPI_URL'),
 });
