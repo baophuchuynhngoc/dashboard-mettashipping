c62313dfcbd9e46822d59fe9411e09ea1aa534c3
Phuc Huynh <baophuchuynhngoc@gmail.com>
2023-07-10 09:10:17 +0700

    Add import export plugin in backend

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,4 +2,10 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  'import-export-entries': {
+    enabled: true,
+    config: {
+      // See `Config` section.
+    },
+  },
 });
